f83275394059b34512122c23db499e7efe788bef
kubo <eq.intern001@gmail.com>
2015-05-29 14:59:34 +0900

    クロスドメイン対策

--- www/js/index.js
+++ www/js/index.js
@@ -9,14 +9,16 @@ var app = {
   // 'load', 'deviceready', 'offline', and 'online'.
   bindEvents: function() {
     document.addEventListener('deviceready', this.onDeviceReady, false);
-    var module = ons.bootstrap('myApp', ['onsen']);
+    var module = ons.bootstrap('myApp', ['onsen','newsLoader']);
     module.controller('AppController', function($scope) {
       console.log("onsen is ready");
     });
-    module.controller('NewsController', function($scope, $http) {
+    module.controller('NewsController', function($scope, nana) {
       ons.ready(function() {
         
         $scope.networkTest = checkConnection();
+        nana.get_news();
+        //console.log("onsen is ready");
       });
     });
     module.controller('PageController', function($scope) {});
@@ -51,19 +53,32 @@ var app = {
 
 //ネットワーク接続種別確認関数
 function checkConnection() {
-            var networkState = navigator.connection.type;
+  var networkState = navigator.connection.type;
 
-            var states = {};
-            states[Connection.UNKNOWN]  = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI]     = 'WiFi connection';
-            states[Connection.CELL_2G]  = 'Cell 2G connection';
-            states[Connection.CELL_3G]  = 'Cell 3G connection';
-            states[Connection.CELL_4G]  = 'Cell 4G connection';
-            states[Connection.CELL]     = 'Cell generic connection';
-            states[Connection.NONE]     = 'No network connection';
+  var states = {};
+  states[Connection.UNKNOWN]  = 'Unknown connection';
+  states[Connection.ETHERNET] = 'Ethernet connection';
+  states[Connection.WIFI]     = 'WiFi connection';
+  states[Connection.CELL_2G]  = 'Cell 2G connection';
+  states[Connection.CELL_3G]  = 'Cell 3G connection';
+  states[Connection.CELL_4G]  = 'Cell 4G connection';
+  states[Connection.CELL]     = 'Cell generic connection';
+  states[Connection.NONE]     = 'No network connection';
 
-            alert('Connection type: ' + states[networkState]);
-        }
+  alert('Connection type: ' + states[networkState]);
+}
+
+angular.module('newsLoader',[]).service('nana', ['$http', function ($http) {
+  this.get_news = function(){
+    return $http({method: "POST"}).jsonp("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%20%3D%20'http%3A%2F%2Fwww.mizukinana.jp%2Fnews%2Findex.html'%20and%20xpath%20%3D%20'%2F%2Fh3'%3B&format=json&diagnostics=true").then(
+      function (response) {
+        return response.data;
+    },
+    function (response) {
+        alert(response.data.message);
+        //return {items: []};
+    });
+  };  
+}]);
 
 app.initialize();
